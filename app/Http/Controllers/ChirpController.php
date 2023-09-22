eeab7483c729f094e4ad5fd3a7c6f5775e468e40
Gleison Lemos <gleison04lemos@gmail.com>
2023-09-21 22:30:39 -0300

    chore: creating routes, migrations and controller chirps, and setting routing

---
+++ app/Http/Controllers/ChirpController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+class ChirpController extends Controller
+{
+    public function index(): Response
+    {
+        return response('Hello, World!');
+    }
+}
